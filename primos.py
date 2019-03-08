aa00bc0b5836793baa5ef2c277ffc3bcbc8eea21
valquero779 <andresserrano2002@hotmail.com>
2019-03-08 12:37:50 -0500

    Ejercicio resuelto

--- primos.py
+++ primos.py
@@ -1,4 +1,10 @@
 a = int(input("valor del numerador: "))
-
+b=0
 for i in range(1, a + 1):
     print("la division de:", a, i, "es:", a / i)
+    if a%i==0:
+        b+=1
+if b>2:
+    print("el numero no es primo")
+else:
+    print("el numero es primo")
