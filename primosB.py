4c662953c93557f1fba41d8e83eeb8f859c62a40
valquero779 <andresserrano2002@hotmail.com>
2019-03-08 14:11:27 -0500

    ejercicio 2 resuelto(correccion)

--- primosB.py
+++ primosB.py
@@ -1,11 +1,13 @@
 a=int(input("Valor deseado: "))
 b=1
 c=0
-while b>-1 and b<a:
-    print("la division entre los digitos: ",a,b,"es: ",a/(b))
-    b+=1
+while  b<a+1:
+
+
     if a%b==0:
         c+=1
+
+    b+=1
 
 if c>2:
     print("El numero no es primo")
